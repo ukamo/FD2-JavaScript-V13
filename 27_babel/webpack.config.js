6f38c2de6856f432549a17e95a54f7f8d1e09734
loktevalexey <loktev.alex.74@gmail.com>
2018-01-21 22:35:52 +0300

    babel

---
+++ 27_babel/webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+
+    entry: './main.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlPlugin()
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+
+};
