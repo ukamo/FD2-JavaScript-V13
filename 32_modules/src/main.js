f1c240be9c4f04784260292275b1c17aae31fea6
loktevalexey <loktev.alex.74@gmail.com>
2018-01-22 11:03:34 +0300

    modules

---
+++ 32_modules/src/main.js
@@ -0,0 +1,5 @@
+import './style1.css';
+
+import {quadra} from './second.js'
+
+alert( quadra(5) );
---
+++ 32_modules/webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isDevelopment = !process.env.production;
+const assetsPath = path.join(__dirname, '/public');
+
+const extractCSS = new ExtractTextPlugin({
+    filename: "bundle.css"
+});
+
+const extractSass = new ExtractTextPlugin({
+    filename: '[name].css',
+    disable: isDevelopment
+});
+
+const config = {
+    entry: {
+        main: './src/main.js'
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+      rules: [
+        {
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            }]
+        }, 
+        {
+            test: /\.css$/,
+            use: extractCSS.extract({
+                use: ["css-loader"]
+            })
+        },
+        {
+            test: /\.scss$/,
+            exclude: [/node_modules/],
+            use: extractSass.extract({
+                fallback: 'style-loader',
+                //resolve-url-loader may be chained before sass-loader if necessary
+                use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: !isDevelopment
+                        }
+                    },
+                    'sass-loader',
+                    'resolve-url-loader'
+                ]
+            })
+        },
+        {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'images/[name][hash].[ext]'
+                    }
+                }, {
+                    loader: 'image-webpack-loader',
+                    options: {
+                        mozjpeg: {
+                            progressive: true,
+                            quality: 70
+                        }
+                    }
+                },
+            ],
+        },
+        {
+            test: /\.(eot|svg|ttf|woff|woff2)$/,
+            use: {
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name][hash].[ext]'
+                }
+            },
+        }
+      ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            _: 'lodash'
+        }),
+        extractSass
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000
+    }
+};
+
+if (isDevelopment) {
+    fs.readdirSync(assetsPath)
+        .map((fileName) => {
+            if (['.css', '.js'].includes(path.extname(fileName))) {
+                return fs.unlinkSync(`${assetsPath}/${fileName}`);
+            }
+
+            return '';
+        });
+} else {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                drop_console: true,
+                unsafe: true
+            }
+        })
+    );
+}
+
+module.exports = config;
